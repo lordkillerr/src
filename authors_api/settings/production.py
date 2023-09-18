3bb21177279b66f80b8ec197c91a5a6473327dc9
lordkillerr <shudhanshuharsh670@gmail.com>
2023-09-18 12:54:24 +0530

    chore:setup basic django project

---
+++ authors_api/settings/production.py
@@ -0,0 +1,7 @@
+from .base import * #noqa
+from .base import env
+
+ADMINS = [("Harsh sudhanshu", "api.shudhanshuharsh670@gmail.com")]
+
+# TODO add domain name of the production server
+CSRF_TRUSTED_ORIGINS = []
